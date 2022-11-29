ee66dee66a75bb0645d297a3a8fce4fb183c0539
Ajay Pokharel <ajaypokharel8@gmail.com>
2022-11-29 00:53:59 -0600

    [Add] Adds built-in functions

--- src/runtime.js
+++ src/runtime.js
@@ -1,3 +1,28 @@
 function stdout(...args) {
   console.log(...args);
 }
+
+function sum(...args) {
+  return args.reduce((sum, num) => sum + num, 0);
+}
+
+function mod(x, y) {
+  return x % y;
+}
+
+function pow(n, m) {
+  return Math.pow(n, m);
+}
+
+function max(...args) {
+  return Math.max(...args);
+}
+
+function min(...args) {
+  return Math.min(...args);
+}
+
+function len(input) {
+  return input.length;
+}
+
